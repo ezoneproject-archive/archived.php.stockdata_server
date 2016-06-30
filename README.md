df63a9ac191453f882af02b4ef406d833c9f8d38
Yotta <yotta38@yahoo.com>
2016-06-30 18:25:09 +0900

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# stockdata_server_php
-stockdata server with php
+# 종목데이터분석 서버
+
+종목데이터분석 서버 프로그램
+
+## Build
+
+* PHP 5.3 or higher (5.3.3 tested)
+    * curl.so
+    * dom.so
+    * fileinfo.so
+    * gd.so
+    * json.so
+    * mbstring.so
+    * mysql.so
+    * mysqli.so
+    * pdo.so
+    * pdo_mysql.so
+    * pdo_sqlite.so
+    * phar.so
+    * sqlite3.so
+    * wddx.so
+    * xmlreader.so
+    * xmlwriter.so
+    * xsl.so
+    * zip.so
+* Apache2 httpd server
+    * mod_rewrite.so
+* Mysql or MariaDB
+
+## Author
+
+Yotta
+
+Distributed under the GPLv3 license. See ``LICENSE`` for more information.
+
+[https://github.com/yottacho/stockdata_server_php](https://github.com/yottacho/stockdata_server_php)
--- api/0.9/trans/trans.api.php
+++ api/0.9/trans/trans.api.php
@@ -1,4 +1,5 @@
 <?php
+require_once "master/stcode.api.php";
 
 // ---------------------------------------------------------------- //
 // 자료 마스터 목록 또는 상세정보 분기
@@ -58,6 +59,7 @@ function api_get_samdata($request) {
 
 // ---------------------------------------------------------------- //
 // 자료 등록 처리
+// 자료 등록시에는 반드시 sam_key/date/time 가 지정되어야 한다
 // @return array(reportkey => reportname)
 // ---------------------------------------------------------------- //
 function api_create_samdata($request) {
@@ -66,22 +68,129 @@ function api_create_samdata($request) {
     $resource_info = $request['_metadata']['ResourceInfo'];
     $access_id = $request['_metadata']['ApiAccessId'];
 
+    if (!isset($resource_info) || strlen($resource_info) == 0)
+        die2(404, "Resource has not found.");
+
     // resource_info = "sam_key/date/time"
-    // 하나라도 어긋나면 die
     //echo $resource_info;
+    @list($sam_key, $data_date, $data_time) = explode("/", $resource_info, 3);
+    if (!isset($sam_key, $data_date, $data_time))
+        die2(400, "Resource format error.", $resource_info);
 
-    //validate_sammast_id($access_id, $sam_key);
+    if (strlen($data_date) != 8)
+        die2(400, "Resource format error. Date is 'YYYYMMDD'.", $resource_info);
 
+    // sam_key 사용권한 확인
+    validate_sammast_id($access_id, $sam_key);
+
+    // data_time 확인
+    validate_samtime($sam_key, $data_time);
+
+    $result = array('FieldNotFound' => "", 'Rows' => 0, 'ErrorData' => array());
+    $rowcnt = 0;
     foreach ($request['post_data'] as $row) {
+        // 필수항목 추출
+        $stockName = search_array_name($row, "종목명");    // 종목명
+        $stockCode = search_array_name($row, "종목코드");  // 종목코드
+        $stockCate = search_array_name($row, "소속업종");  // 소속업종
+
+        if (strlen($stockCode) == 0)
+            die2(400, "Data format error. Can't found Stock code.");
+        if (strlen($stockName) == 0)
+            die2(400, "Data format error. Can't found Stock name.");
+
+        // 종목검색
+        $stcode_res = get_stcode_stcode($stockCode);
+        if (count($stcode_res) == 0) {
+            // 미등록 종목코드 => 신규등록
+            insert_stcode($stockCode, $stockName, $stockCate);
+        }
+
+        $colcnt = 0;
         foreach ($row as $column) {
             $fieldName = $column['FieldName'];
             $fieldValue = $column['FieldValue'];
 
+            if (strcmp($fieldName,"종목명") == 0 ||
+                strcmp($fieldName,"종목코드") == 0 ||
+                strcmp($fieldName,"소속업종") == 0) {
+                continue;
+            }
 
+            // 필드검색
+            $item_key = get_item_key_from_name($sam_key, $fieldName);
+            if ($item_key < 0) {
+                // 없는 필드명
+                if (strpos(@$result['FieldNotFound'], $fieldName) === FALSE) {
+                    if (strlen(@$result['FieldNotFound']) > 0)
+                        @$result['FieldNotFound'] .= ",";
+                    @$result['FieldNotFound'] .= $fieldName;
+                }
+                continue;
+            }
+
+            // 자료 등록
+            if ($stmt = @$DB_CONN->prepare("INSERT INTO SAMDATA (GSDATE, GSTIME, SAM_KEY, STCODE, ITEM_KEY, DATA) VALUES (?, ?, ?, ?, ?, ?)")) {
+                $stmt->bind_param("ssisis", $data_date, $data_time, $sam_key, $stockCode, $item_key, $fieldValue);
+                if (!$stmt->execute()) {
+                    $stmt->close();
+
+                    // ErrorData 별도관리
+                    $result['ErrorData'][] = array(
+                        'StockName' => $stockName,
+                        'FieldName' => $fieldName,
+                        'FieldValue' => $fieldValue,
+                    );
+                }
+                else {
+                    $stmt->close();
+                    $colcnt++;
+                }
+            }
+            else
+                die2(500, "Internal Server Error (query:api_create_samdata)", $DB_CONN->error);
         }
-    }
 
-    return array();
+        if ($colcnt > 0)
+            $rowcnt++;
+    }
+    $result['Rows'] = $rowcnt;
+
+    return $result;
+}
+
+// 필드속성에서 item_key를 가져온다
+function get_item_key_from_name($sam_key, $name) {
+    global $DB_CONN;
+
+    $item_key = -1;
+    if ($stmt = @$DB_CONN->prepare("SELECT ITEM_KEY FROM SAMSTRUCT WHERE SAM_KEY = ? AND ITEM_NAME = ?")) {
+        $stmt->bind_param("is", $sam_key, $name);
+        $stmt->execute();
+
+        $stmt->bind_result($r_item_key);
+        if ($stmt->fetch())
+        {
+            $item_key = $r_item_key;
+        }
+        $stmt->close();
+
+        return $item_key;
+    }
+    else
+        die2(500, "Internal Server Error (query:validate_sammast_id)", $DB_CONN->error);
+}
+
+// 필드 어레이에서 값을 추출한다
+function search_array_name($arr, $value) {
+    foreach ($arr as $item) {
+        $fieldName = $item['FieldName'];
+        $fieldValue = $item['FieldValue'];
+
+        if (strcmp($fieldName, $value) == 0)
+            return $fieldValue;
+    }
+    return "";
 }
 
 // api access id 와 sammast 검증 (sammast 에서 해당 테이블 이용 가능한지 검증)
@@ -96,7 +205,7 @@ function validate_sammast_id($access_id, $sam_key) {
         if (!$stmt->fetch())
         {
             $stmt->close();
-            die2(404, "Can't found report id.", $accessid.":".$sam_key);
+            die2(404, "Can't found report id.", $access_id.":".$sam_key);
         }
         $stmt->close();
 
@@ -105,3 +214,28 @@ function validate_sammast_id($access_id, $sam_key) {
     else
         die2(500, "Internal Server Error (query:validate_sammast_id)", $DB_CONN->error);
 }
+
+// sam_key 에 대해 time 값 확인
+// access_id 검증하지 않음
+function validate_samtime($sam_key, $data_time) {
+    global $DB_CONN;
+
+    if ($stmt = @$DB_CONN->prepare("SELECT TIME_DISPLAY FROM SAMTIME WHERE SAM_KEY = ? AND TIME_VALUE = ?")) {
+        $stmt->bind_param("is", $sam_key, $data_time);
+        $stmt->execute();
+
+        $stmt->bind_result($r_time_display);
+        if (!$stmt->fetch())
+        {
+            $stmt->close();
+            die2(404, "Resource format error. Time not found.", $sam_key.":".$data_time);
+        }
+        $stmt->close();
+
+        return;
+    }
+    else
+        die2(500, "Internal Server Error (query:validate_samtime)", $DB_CONN->error);
+}
+
+
